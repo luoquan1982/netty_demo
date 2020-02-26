a83d51d0eb7e93071db404b639273223c0d12d97
luoquan <luo.quan1982@gmail.com>
2020-02-26 14:28:06 +0800

    bio服务器端程序

--- src/main/java/com/luoquan/bio/BIOServer.java
+++ src/main/java/com/luoquan/bio/BIOServer.java
@@ -1,5 +1,12 @@
 package com.luoquan.bio;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * BIOServer
  *
@@ -7,4 +14,56 @@ package com.luoquan.bio;
  * @date 2020/2/26 13:35
  */
 public class BIOServer {
+    public static void main(String[] args) throws Exception {
+        // 线程池机制
+        //1、创建一个线程池
+        //2、如果有客户端连接就创建一个线程与之通信(单独写一个方法)
+        ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
+
+        //创建ServerSocket
+        ServerSocket serverSocket = new ServerSocket(666);
+
+        System.out.println("服务器启动了");
+        while (true) {
+            //监听,等待客户端连接
+            final Socket socket = serverSocket.accept();
+            System.out.println("连接到一个客户端");
+
+            //创建一个线程,与之通信(单独写一个方法)
+            newCachedThreadPool.execute(() -> {
+                //可以和客户端通讯
+                handler(socket);
+            });
+        }
+    }
+
+    //编写一个handler方法,和客户端通信
+    public static void handler(Socket socket) {
+        try {
+            System.out.println("线程信息 ID: " + Thread.currentThread().getId() + " 名字:" + Thread.currentThread().getName());
+            byte[] bytes = new byte[1024];
+            //通过socket获取一个输入流
+            InputStream inputStream = socket.getInputStream();
+            //循环读取客户端发送的数据
+            while (true) {
+                System.out.println("线程信息 ID: " + Thread.currentThread().getId() + " 名字:" + Thread.currentThread().getName());
+                int read = inputStream.read(bytes);
+                if (-1 != read) {
+                    //输出客户端发送的数据
+                    System.out.println(new String(bytes, 0, read));
+                } else {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("关闭与clilent的连接");
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
