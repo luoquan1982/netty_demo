2b4c5c3c7c324ffb0c8c032a64baaa00c4dff1e3
luo_quan <37629327@qq.com>
2020-03-02 17:27:33 +0800

    一个简单的netty实现的http服务器端

--- src/main/java/com/luoquan/netty/http/TestHttpServerHandler.java
+++ src/main/java/com/luoquan/netty/http/TestHttpServerHandler.java
@@ -1,7 +1,55 @@
 package com.luoquan.netty.http;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.net.URI;
+
 /**
+ * 1.SimpleChannelInboundHandler是ChannelInboundHandlerAdapter的子类
+ * 2.HttpObject表示的是客户端和服务器端相互通信的数据被封装程HttpObject
+ *
  * @author luoquan
  */
-public class TestHttpServerHandler {
+public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    //channelRead0 读取客户端数据
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+
+        //判断msg是不是httpRequest请求
+        if (msg instanceof HttpRequest) {
+
+            System.out.println("pipeline hashcode:" + ctx.pipeline().hashCode() + "TestHttpServerHandler hash:" + this.hashCode());
+
+            System.out.println("msg类型是:" + msg.getClass());
+            System.out.println("客户端地址:" + ctx.channel().remoteAddress());
+
+            //获取到
+            HttpRequest httpRequest = (HttpRequest) msg;
+
+            //获取uri,过滤指定的资源
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("你请求了favicon.icon,不做响应");
+                return;
+            }
+
+            //回复信息给浏览器[http协议]
+            ByteBuf content = Unpooled.copiedBuffer("hello,from netty server", CharsetUtil.UTF_8);
+
+            //构造一个http的响应,即httpResponse
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+            //将构建好的response返回
+            ctx.writeAndFlush(response);
+        }
+    }
 }
--- src/main/java/com/luoquan/netty/http/TestServerInitializer.java
+++ src/main/java/com/luoquan/netty/http/TestServerInitializer.java
@@ -1,7 +1,29 @@
 package com.luoquan.netty.http;
 
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
 /**
  * @author luoquan
  */
-public class TestServerInitializer {
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+
+        //向管道加如处理器
+        //得到管道
+        ChannelPipeline pipeline = ch.pipeline();
+
+        //加如一个netty提供的httpServerCodec codec=>[code - decoder]
+        //HttpServerCodec说明
+        //1.HttpServerCodec是netty提供的处理http的编-解码器
+        pipeline.addLast("MyHttpServerCodec", new HttpServerCodec());
+        //2.增加一个自定义的handler
+        pipeline.addLast("MyTestHttpServerHandler", new TestHttpServerHandler());
+
+        System.out.println("ok~~~");
+    }
 }
